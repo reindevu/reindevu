d98db776f55a4c2689505c577bacd68630c930ba
Pavel Yakovlev <thrnrhhl@yandex.ru>
2025-05-22 01:25:37 +0300

    UNT-21 [reindevu] Update the design

--- src/entities/resume/ui/resume-about-me/ui.tsx
+++ src/entities/resume/ui/resume-about-me/ui.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
 import { FC } from "react";
 import Image1 from "./assets/image-1.png";
+import Image2 from "./assets/image-2.png";
 
 export const ResumeAboutMe: FC = () => {
   return (
     <>
-      <div className="my-5 flex flex-col items-center gap-10 md:w-11/12 md:flex-row">
+      <div className="my-5 flex flex-col gap-5 items-center w-full md:flex-row">
         <Image
-          src="/me.png"
+          src={Image2.src}
           alt="Me"
-          width="220"
-          height="550"
-          className="shadow-2xl"
+          width={Image2.width/4}
+          height={Image2.height/4}
+          className="shadow-2xl w-full lg:w-auto h-[15.625rem] lg:h-auto object-cover"
         />
 
-        <p className="text:sm whitespace-pre-line text-justify font-black uppercase text-black dark:text-white lg:text-lg">
+        <p className="text-xs whitespace-pre-line text-justify font-black uppercase text-black dark:text-white lg:text-lg">
           Я — фронтенд-разработчик, реализующий системы учёта, интернет-магазины
           и лендинги под любые бизнес-задачи. В работе применяю React, Next.js,
           Tailwind CSS, TypeScript, Redux Toolkit и Zustand, а также гибко
